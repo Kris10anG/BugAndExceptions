2795978cb2c47cbfa00ce38650f68d751ce81a5c
Kris10anG <kristiang@getacademy.no>
2022-06-02 14:09:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BugAndExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            var shop = new Shop();
+            var customer = new Customer("Kristian", new Address() {City = "Larvik"},
+                new BankInformation() {Balance = 100});
+            while (true)
+            {
+                shop.PrintItems();
+                Console.WriteLine();
+                Console.WriteLine("What do you want to buy?");
+                Console.WriteLine("if finished shopping, write 0");
+                Console.WriteLine();
+
+                var itemNumber = Console.ReadLine();
+                if (itemNumber == "0")
+                {
+                    shop.CheckOut(customer);
+                }
+                else
+                {
+                    customer.AddItemToShoppingCart(shop.ShopItems.Find(item => item.Id == itemNumber));
+                    customer.PrintItemsInCart();
+                }
+            }
+        }
+    }
+}
